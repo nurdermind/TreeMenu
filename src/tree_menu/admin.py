0493a8525c68da7da5875c7e657194af5c81fa21
tm.kk23 <mansur.tumashev@gmail.com>
2023-03-04 13:30:09 +0500

    add models

--- src/tree_menu/admin.py
+++ src/tree_menu/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from .models import TreeLink
 
 # Register your models here.
+admin.site.register(TreeLink)
--- src/tree_menu/models.py
+++ src/tree_menu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class TreeLink(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.title
