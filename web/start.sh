dbebe7ffd3a711543d6b446a506c37bcc42cadec
tm.kk23 <mansur.tumashev@gmail.com>
2023-03-06 23:28:43 +0500

    fix cmd

--- web/start.sh
+++ web/start.sh
@@ -1,5 +1,5 @@
 !#/bin/bash
-python manage.py migrate --no-input
+python manage.py migrate --noinput
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 python manage.py collectstatic --noinput
 gunicorn UpTraderTZ.wsgi:application --bind 0.0.0.0:8000 -w 4
